ebda14d194f9a167ee825e33a92e0f71cf8749f1
Almad <bugs@almad.net>
2018-05-28 23:48:47 +0200

    Use localhost as default for CI

--- graveyard/settings.py
+++ graveyard/settings.py
@@ -78,7 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dracidoupe_cz',
+        'TEST_DATABASE_NAME': 'circle_test',
         'USER': 'root',
+        'HOST': '127.0.0.1',
         'PASSWORD': '',
         'OPTIONS': {
             'charset': 'latin2'
