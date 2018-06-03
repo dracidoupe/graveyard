4c631eb4662755a55db1cc6a6610bb4d608030ca
Almad <bugs@almad.net>
2018-06-03 18:35:41 +0200

    Namespace DDCZ app

--- ddcz/urls.py
+++ ddcz/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name='ddcz'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='news'),
 ]
--- graveyard/urls.py
+++ graveyard/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('ddcz.urls')),
+    path('', include('ddcz.urls', namespace='ddcz')),
     path('admin/', admin.site.urls),
 ]
