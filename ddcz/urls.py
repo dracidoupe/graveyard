6528d50acccee4edefc49e48baa4d763e701c0e1
Almad <bugs@almad.net>
2018-06-04 00:16:32 +0200

    Append slash to URLs properly

--- ddcz/urls.py
+++ ddcz/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('rubriky/clanky/', views.common_articles, {
         'creative_page_slug': 'clanky'
     }, name='articles-and-essays'),
-    path('rubriky/<creative_page_slug>/<int:article_id>-<article_slug>', views.common_article_detail, name='common-article-detail'),
+    path('rubriky/<creative_page_slug>/<int:article_id>-<article_slug>/', views.common_article_detail, name='common-article-detail'),
 ]
--- graveyard/settings/base.py
+++ graveyard/settings/base.py
@@ -91,15 +91,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'cs-CZ'
-
 TIME_ZONE = 'Europe/Prague'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
+APPEND_SLASH=True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
