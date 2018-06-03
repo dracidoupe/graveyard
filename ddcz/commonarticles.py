493760379947d42cca69d79c18f2b72875639c0a
Almad <bugs@almad.net>
2018-06-04 00:51:13 +0200

    Display all common articles

--- ddcz/commonarticles.py
+++ ddcz/commonarticles.py
@@ -34,13 +34,106 @@ COMMON_ARTICLES_CREATIVE_PAGES = {
             'Specializace pro pokročilé',
             'Začátečnické povolání',
         ]
-    }
+    },
+    'noverasy': {
+        'name': 'Nové Rasy',
+        'sections': [
+            'Kříženec',
+            'Nová rasa',
+            'Odrůda',
+            'Ostatní,'
+        ]
+    },
+    # Not a common article, in fact
+    # 'predmety': {
+    #     'name': 'Předměty',
+    #     'sections': [
+    #         'Zbraně',
+    #         'Zbroj',
+    #         'Artefakty',
+    #         'Kouzelné zbraně',
+    #         'Kouzelná zbroj',
+    #         'Kouzelné předměty',
+    #         'Obyčejné předměty',
+    #     ]
+    # },
+    'expanze': {
+        'name': 'Rozvoj DrD',
+        'sections': [
+            'Alternativní pravidla',
+            'Armády a vojenství',
+            'Ekonomika a obchod',
+            'Nadpřirozeno',
+            'Ostatní',
+            'Politika a diplomacie',
+            'Světy a prostředí',
+            'Teorie RPG',
+        ]
+    },
+    'alchymistazvl': {
+        'name': 'Alchymista',
+        'sections': [
+            'Alchymista',
+            'Ostatní',
+            'Pyrofor',
+            'Theurg',
+        ]
+    },
+    'hranicar': {
+        'name': 'Hraničář',
+        'sections': [
+            'Chodec',
+            'Druid',
+            'Hraničář',
+            'Ostatní',
+        ]
+    },
+    'kouzelnikzvl': {
+        'name': 'Kouzelník',
+        'sections': [
+            'Čaroděj',
+            'Kouzelník',
+            'Mág',
+            'Ostatní',
+            'Přítel',
+
+        ]
+    },
+    'valecnik': {
+        'name': 'Válečník',
+        'sections': [
+            'Alternativní pravidla',
+            'Bojovník',
+            'Ostatní',
+            'Válečník',
+            'Šermíř',
+        ]
+    },
+    'zlodej': {
+        'name': 'Zloděj',
+        'sections': [
+            'Lupič',
+            'Ostatní',
+            'Sicco',
+            'Zloděj',
+            'Zlodějský předmět',
+        ]
+    },
 }
 
 
 SLUG_NAME_TRANSLATION_FROM_CZ = {
     'clanky': 'articles-and-essays',
     'novapovolani': 'new-classes',
+    'noverasy': 'new-races',
+    # 'predmety': 'items',
+    'expanze': 'extending-rules',
+    'alchymistazvl': 'alchemist',
+    'hranicar': 'ranger',
+    'kouzelnikzvl': 'wizard',
+    'valecnik': 'warrior',
+    'zlodej': 'rogue',
+
 }
 
 SLUG_NAME_TRANSLATION_TO_CZ = {v:k for k,v in SLUG_NAME_TRANSLATION_FROM_CZ.items()}
