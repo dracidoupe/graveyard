167f31fcc438d5747407f70565497df5e657a4f5
Almad <bugs@almad.net>
2018-06-03 23:50:29 +0200

    Create initial (unmanaged) migrations

--- graveyard/settings/production.example.py
+++ graveyard/settings/production.example.py
@@ -1,5 +1,4 @@
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'xoxo, but longer and on production'
 
@@ -14,7 +13,19 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'ddcz-graveyard',
+    }
+}
 
 WSGI_APPLICATION = 'graveyard.wsgi.application'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nove.dracidoupe.cz']
+
+STATIC_ROOT = '/var/www/dracidoupe.cz/www_root/static/htdocs/'
+
+STATIC_URL = 'https://static.dracidoupe.cz/'
+
+STATICFILE_DIRS = []
