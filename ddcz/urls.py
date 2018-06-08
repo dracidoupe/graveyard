8da134823a56567e09a09aefc44837a837644912
Almad <bugs@almad.net>
2018-06-08 22:02:27 +0200

    Put news on non-root path for clarity

--- ddcz/urls.py
+++ ddcz/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 app_name='ddcz'
 
 urlpatterns = [
-    path('', views.index, name='news'),
+    path('', RedirectView.as_view(url='aktuality/', permanent=True)),
+    path('aktuality/', views.index, name='news'),
     path('rubriky/<creative_page_slug>/', views.common_articles, name='common-article-list'),
     path('rubriky/<creative_page_slug>/<int:article_id>-<article_slug>/', views.common_article_detail, name='common-article-detail'),
 
