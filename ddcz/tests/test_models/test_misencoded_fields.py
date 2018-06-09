5d189e615d5944967fd564383302e0449ba0f81b
Almad <bugs@almad.net>
2018-06-09 19:57:36 +0200

    Cleanup

--- ddcz/tests/test_models/test_misencoded_fields.py
+++ ddcz/tests/test_models/test_misencoded_fields.py
@@ -20,17 +20,11 @@ class AnimalTestCase(TestCase):
         self.assertEquals(self.problematic_diacritic_string, p.nick_uzivatele)
 
     def test_set_save_doesnt_destroy_database(self):
-        # UserProfile.objects.create(
-        #     nick_uzivatele = self.problematic_diacritic_string
-        # )
-
-        p = UserProfile(
+        p = UserProfile.objects.create(
             nick_uzivatele = self.problematic_diacritic_string
         )
-        p.save()
 
         d = UserProfile.objects.get(id=p.id).nick_uzivatele
-        #   print('decoded: ' + d.encode('latin2').decode('cp1250'))
     
         self.assertEquals(
             self.problematic_diacritic_string,
@@ -45,11 +39,6 @@ class NewsTestCase(TestCase):
         self.problematic_diacritic_string = "ěščřžýáíéďťň"
 
     def test_save_and_read(self):
-        # datum = models.DateTimeField()
-        # autor = MisencodedTextField()
-        # autmail = MisencodedTextField()
-        # text = MisencodedTextField()
-
         p = News(
             datum = datetime.now(pytz.timezone('Europe/Prague')),
             autor = "xoxo",
