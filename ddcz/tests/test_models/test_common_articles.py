8f623226348cf97fc4e887b0ebdcc8d8f74548f5
Almad <bugs@almad.net>
2018-06-14 23:43:33 +0200

    Fix tests

--- ddcz/tests/test_models/test_common_articles.py
+++ ddcz/tests/test_models/test_common_articles.py
@@ -1,20 +1,20 @@
 from unittest import TestCase
 
-from ddcz.models import CommonArticles
+from ddcz.models import CommonArticle
 
 class TestSlugGenerator(TestCase):
     def test_simple(self):
-        a = CommonArticles(jmeno='ahoj a hola')
+        a = CommonArticle(jmeno='ahoj a hola')
         self.assertEquals('ahoj-a-hola', a.get_slug())
 
     def test_upper(self):
-        a = CommonArticles(jmeno='ahoj a HOLA')
+        a = CommonArticle(jmeno='ahoj a HOLA')
         self.assertEquals('ahoj-a-hola', a.get_slug())
 
     def test_combining(self):
-        a = CommonArticles(jmeno='ahoj a ďíky')
+        a = CommonArticle(jmeno='ahoj a ďíky')
         self.assertEquals('ahoj-a-diky', a.get_slug())
 
     def test_strip_noncombining(self):
-        a = CommonArticles(jmeno='ahoj a 你好')
+        a = CommonArticle(jmeno='ahoj a 你好')
         self.assertEquals('ahoj-a', a.get_slug())
