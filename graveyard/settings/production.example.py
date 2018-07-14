d4fcd7e72ea0797747bfacca1a29300d82ed64c2
Almad <bugs@almad.net>
2018-07-14 11:43:35 +0200

    Add sentry config (refs #16)

--- graveyard/settings/production.example.py
+++ graveyard/settings/production.example.py
@@ -28,6 +28,12 @@ STATIC_ROOT = '/var/www/dracidoupe.cz/www_root/static/htdocs/'
 
 STATIC_URL = 'https://static.dracidoupe.cz/'
 
+import raven
+
+RAVEN_CONFIG = {
+    'dsn': 'https://xoxo:xixi@sentry.io/12345678',
+}
+
 # Security settings
 X_FRAME_OPTIONS="DENY"
 CSRF_COOKIE_SECURE=True
