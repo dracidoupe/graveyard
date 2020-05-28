03cdaf99695737eebe5fc9dfeb750632fef0a384
Almad <bugs@almad.net>
2020-05-28 18:42:22 +0200

    feat: Better logging

--- graveyard/settings/base.py
+++ graveyard/settings/base.py
@@ -125,3 +125,41 @@ SELENIUM_HUB_HOST = None
 # If none, rely on default Django settings as we are running locally
 SELENIUM_PROJECT_HOST=None
 SELENIUM_IMPLICIT_WAIT = 10
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                        'pathname=%(pathname)s lineno=%(lineno)s ' +
+                        'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        }
+    }
+}
