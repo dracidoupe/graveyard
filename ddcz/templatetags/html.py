c6cb03048c5d9236f89cc506327db0f51a7051b5
Almad <bugs@almad.net>
2020-06-18 17:21:04 +0200

    feat(CreativePages): Switch to more compliant parser (refs #1)

--- ddcz/templatetags/html.py
+++ ddcz/templatetags/html.py
@@ -1,6 +1,6 @@
 from django import template
 
-from ..html import encode_valid_html
+from ..html import encode_valid_html, unsafe_encode_valid_creation_html
 
 register = template.Library()
 
@@ -8,3 +8,8 @@ def render_html(value):
     return encode_valid_html(value)
 
 register.filter('render_html', render_html)
+
+def render_html_insecurely(value):
+    return unsafe_encode_valid_creation_html(value)
+
+register.filter('render_html_insecurely', render_html)
