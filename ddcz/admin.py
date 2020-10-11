a1d10c078c3da9deece1a1f6aec5540734535e2e
Almad <bugs@almad.net>
2020-10-11 22:08:02 +0200

    feat(creation): Add Alchemist Tools (refs #53)

--- ddcz/admin.py
+++ ddcz/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 
-from .models import CommonArticle, Monster, News, CreativePageConcept
+from .models import (
+    AlchemistTool,
+    CommonArticle, CreativePageConcept,
+    Monster, News, Skill
+)
 
 admin.site.site_header = "Administrace pro DraciDoupe.cz"
 admin.site.site_title = admin.site.site_header
 
-admin.site.register(CommonArticle)
+class CommonArticleAdmin(admin.ModelAdmin):
+    search_fields = ('jmeno',)
+
+class AlchemistToolAdmin(admin.ModelAdmin):
+    search_fields = ('jmeno',)
+
+
+admin.site.register(AlchemistTool, AlchemistToolAdmin)
+admin.site.register(CommonArticle, CommonArticleAdmin)
+admin.site.register(CreativePageConcept)
 admin.site.register(Monster)
 admin.site.register(News)
-admin.site.register(CreativePageConcept)
+admin.site.register(Skill)
