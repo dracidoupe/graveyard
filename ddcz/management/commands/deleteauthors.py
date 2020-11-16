467bd753de5436935adf3c6184261835f04d36e3
Almad <bugs@almad.net>
2020-11-16 15:31:59 +0100

    fix(Authors): Fix UnicodeEncode problems during Author's migration (refs #42)

---
+++ ddcz/management/commands/deleteauthors.py
@@ -0,0 +1,13 @@
+from django.apps import apps
+from django.core.management.base import BaseCommand, CommandError
+
+from ddcz.models import Author
+
+class Command(BaseCommand):
+    help = """
+    Delete all data from Author table and associated references.
+    Will be forbiddn to run on production (at one point)
+    """
+
+    def handle(self, *args, **options):
+        Author.objects.all().delete()
