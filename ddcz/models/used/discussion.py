e96bd5eeeef727fbe4c72f9eb3c21b108dccfbe8
Almad <bugs@almad.net>
2020-11-20 17:49:39 +0100

    feat(Phorum): Display links to profiles for registered users (refs #120)

--- ddcz/models/used/discussion.py
+++ ddcz/models/used/discussion.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from ..magic import MisencodedCharField, MisencodedTextField
+from .users import UserProfile
 
 class Phorum(models.Model):
     nickname = MisencodedCharField(max_length=64)
@@ -10,6 +11,18 @@ class Phorum(models.Model):
     # stands for 'registered', contains either '1' or IP address
     reg = MisencodedCharField(max_length=50)
     reputace = models.IntegerField()
+    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, blank=True, null=True)
 
     class Meta:
         db_table = 'forum'
+
+    @property
+    def user_profile_url(self):
+        if self.user:
+            return self.user.profile_url
+        else:
+            return None
+
+    @property
+    def by_registered_user(self):
+        return self.reg == '1'
---
+++ ddcz/tests/test_models/test_phorum.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+
+from ..model_generator import get_valid_article_chain
+
+from ddcz.models import Phorum
+
+class TestAuthorLinkRender(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        data = get_valid_article_chain()
+
+        self.user = data['user']
+        self.user.save()
+
+        self.comment = Phorum.objects.create(
+            nickname = 'Author',
+            email = 'test@example.com',
+            text = 'Text of the comment',
+            reg = '1',
+            reputace = 0,
+            user = self.user
+        )
+
+
+    def test_name_inferred(self):
+        comment = Phorum.objects.all()[0]
+        self.assertEqual(
+            self.user.profile_url,
+            comment.user_profile_url
+        )
+
