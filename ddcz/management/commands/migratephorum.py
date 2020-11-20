19292673e2f9b2615363952950e8a1ace3a5455f
Almad <bugs@almad.net>
2020-11-20 17:49:39 +0100

    feat(Phorum): Display links to profiles for registered users (refs #120)

---
+++ ddcz/management/commands/migratephorum.py
@@ -0,0 +1,27 @@
+import logging
+
+from django.apps import apps
+from django.core.management.base import BaseCommand, CommandError
+from ddcz.text import misencode
+
+from ddcz.models import (
+    Phorum, UserProfile
+)
+from ddcz.text import misencode
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = 'Fills in reference to User object into Phorum'
+
+    def handle(self, *args, **options):
+        for comment in Phorum.objects.all():
+            if comment.by_registered_user:
+                try:
+                    comment.user = UserProfile.objects.get(
+                        nick_uzivatele = misencode(comment.nickname)
+                    )
+                    comment.save()
+
+                except UserProfile.DoesNotExist:
+                    logger.info("Can't find author for nickname %s" % comment.nickname)
