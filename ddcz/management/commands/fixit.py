9bf1fc7842d3ea340c87864831e17f2ba52f8c84
Almad <almad@dracidoupe.cz>
2021-04-11 13:05:24 +0200

    Try to fix your local database

---
+++ ddcz/management/commands/fixit.py
@@ -0,0 +1,19 @@
+from django.apps import apps
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = """
+    It's broken. Fix it!
+    Runs all recommended migrations. 
+    """
+
+    def handle(self, *args, **options):
+        call_command("migrate")
+        call_command("migrateauthors")
+        call_command("migratecomments")
+        call_command("migratephorum")
+        call_command("loaddata", "pages")
+        call_command("loaddata", "editorarticles")
+        # call_command("loaddata", "creationexamples")
