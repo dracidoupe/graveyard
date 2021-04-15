043316bcaca3a1ab96b682b8b2c53ba2a197851c
Almad <almad@dracidoupe.cz>
2021-04-15 15:58:39 +0200

    Preserve some session keys after logout (fixes #187)

--- ddcz/users.py
+++ ddcz/users.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import logout
 from django.contrib.auth.models import User
+
+SESSION_KEYS_TO_PRESERVE = ["skin"]
 
 
 def migrate_user(profile, password):
@@ -13,6 +16,14 @@ def migrate_user(profile, password):
     profile.user = user
     profile.save()
 
-    # TODO: Presumably, we could fix more things during migration...like FKs.
+    # TODO: Presumably, we could fix more things during migration...like foreign keys.
 
     return user
+
+
+def logout_user_without_losing_session(request):
+    cache = {k: request.session.get(k) for k in SESSION_KEYS_TO_PRESERVE}
+    logout(request)
+    for key in cache:
+        if cache[key]:
+            request.session[key] = cache[key]
