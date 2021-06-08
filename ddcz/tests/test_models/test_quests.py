d62283c50555b237ed76b60f0abbeb599b86cf62
Almad <almad@dracidoupe.cz>
2021-06-08 13:47:35 +0200

    Fix quest tests

--- ddcz/tests/test_models/test_quests.py
+++ ddcz/tests/test_models/test_quests.py
@@ -5,7 +5,7 @@ from ddcz.models import Quest
 
 class TestQuestUrls(SimpleTestCase):
     def test_s3_url(self):
-        a = Quest(pk=1, cesta="index.html", name="Name")
+        a = Quest(pk=1, path="index.html", name="Name")
         self.assertEquals(
             "http://uploady.dracidoupe.cz/dobrodruzstvi/1/index.html", a.get_final_url()
         )
