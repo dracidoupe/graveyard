1a423397578949bdb45fa65e6e2db1f1adcddac9
Almad <almad@dracidoupe.cz>
2021-06-09 18:56:44 +0200

    Add migrations for attribute name translations (refs #54)

--- ddcz/models/used/social.py
+++ ddcz/models/used/social.py
@@ -14,38 +14,38 @@ MARKET_SECTION_CHOICES = (
 
 
 class Dating(models.Model):
-    jmeno = MisencodedCharField(
+    name = MisencodedCharField(
         max_length=40, blank=True, null=True, db_column="jmeno", verbose_name="Jméno"
     )
     email = MisencodedCharField(max_length=40, blank=True, null=True)
-    telefon = MisencodedCharField(
+    phone = MisencodedCharField(
         max_length=20,
         blank=True,
         null=True,
         db_column="telefon",
         verbose_name="Telefon",
     )
-    mobil = MisencodedCharField(
+    mobile = MisencodedCharField(
         max_length=20, blank=True, null=True, db_column="mobil", verbose_name="Mobil"
     )
-    vek = models.IntegerField(
+    age = models.IntegerField(
         blank=True, null=True, db_column="vek", verbose_name="Věk"
     )
-    okres = MisencodedCharField(
+    area = MisencodedCharField(
         max_length=40, blank=True, null=True, db_column="okres", verbose_name="Okres"
     )
-    doba = MisencodedCharField(
+    experience = MisencodedCharField(
         max_length=20,
         blank=True,
         null=True,
         db_column="doba",
         verbose_name="Doba hraní DrD",
     )
-    datum = models.DateTimeField(
+    published = models.DateTimeField(
         blank=True, null=True, db_column="datum", verbose_name="Datum"
     )
     text = MisencodedTextField(blank=True, null=True, db_column="text")
-    sekce = MisencodedCharField(
+    group = MisencodedCharField(
         max_length=20, blank=True, null=True, db_column="sekce", verbose_name="Sekce"
     )
 
@@ -56,36 +56,36 @@ class Dating(models.Model):
 
 
 class Market(models.Model):
-    sekce = MisencodedCharField(
+    group = MisencodedCharField(
         max_length=20,
         choices=MARKET_SECTION_CHOICES,
         db_column="sekce",
         verbose_name="Sekce",
     )
-    jmeno = MisencodedCharField(
+    name = MisencodedCharField(
         max_length=30, blank=True, null=True, db_column="jmeno", verbose_name="Jméno"
     )
     mail = MisencodedCharField(
         max_length=30, blank=True, null=True, db_column="mail", verbose_name="E-mail"
     )
-    telefon = MisencodedCharField(
+    phone = MisencodedCharField(
         max_length=15,
         blank=True,
         null=True,
         db_column="telefon",
         verbose_name="Telefon",
     )
-    mobil = MisencodedCharField(
+    mobile = MisencodedCharField(
         max_length=15, blank=True, null=True, db_column="mobil", verbose_name="Mobil"
     )
-    okres = MisencodedCharField(
+    area = MisencodedCharField(
         max_length=20, blank=True, null=True, db_column="okres", verbose_name="Okres"
     )
-    text = MisencodedTextField(db_column="text")
+    text = MisencodedTextField()
     # WARNING WARNING WARNING, not a Date, but a varchar instead!
     # Old version stores in the Czech format: dd. mm. YYYY (where d/m is without leading 0)
     # See https://github.com/dracidoupe/graveyard/issues/195
-    datum = MisencodedCharField(
+    published_varchar = MisencodedCharField(
         max_length=12, db_column="datum", verbose_name="Přidáno"
     )
 
