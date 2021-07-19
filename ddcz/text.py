b7a645ea6b4fc21b44f45ccf19875f752924cdaa
Almad <almad@dracidoupe.cz>
2021-07-19 17:50:13 +0200

    Escape user input in a way compatible with the old version (refs #320)

--- ddcz/text.py
+++ ddcz/text.py
@@ -1,6 +1,8 @@
 import re
 from unicodedata import normalize, combining
 
+from django.utils.html import escape
+
 
 def create_slug(text):
     slug = normalize("NFD", text)
@@ -19,3 +21,28 @@ def misencode(text):
     represented by MisencodedChar/TextField.
     """
     return text.encode("cp1250").decode("latin2")
+
+
+def escape_user_input(text, newline_to_br=True):
+    """
+    Escape user input from a text form in a way that's compatible with the v1 version
+
+    Original code:
+    function modifystring($string,$wrap=0)
+    {
+        //$retezec=addslashes($retezec);
+        $string=HTMLSpecialChars($string);
+        If($zalomit==1):
+          $string=eregi_replace("\n","<br />",$string);
+        EndIf;
+
+        return $retezec;
+    }
+
+    """
+
+    text = escape(text)
+    if newline_to_br:
+        text = text.replace("\n", "<br />")
+
+    return text
