7d55f792005a7f08dac1ea7f366e400833b1ce64
Almad <almad@dracidoupe.cz>
2021-07-22 17:44:44 +0200

    Test phorum with the browser against XSS (refs #320)

--- ddcz/tests/test_ui/cases.py
+++ ddcz/tests/test_ui/cases.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import socket
 
 from django.conf import settings
@@ -5,13 +6,30 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.utils.decorators import classproperty
 
 from selenium import webdriver
-from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class MainPage(Enum):
+    BODY = "//body"
+    MAIN_TITLE = "//h1[contains(@class, 'page-heading')]"
+    LOGIN_USERNAME_INPUT = '//*[@id="id_nick"]'
+    LOGIN_PASSWORD_INPUT = '//*[@id="id_password"]'
+    LOGIN_SUBMIT = '//*[@id="login_submit"]'
+    LOGOUT_SUBMIT = '//*[@id="logout_submit"]'
+    CONTROL_NICK = '//*[@id="ddcz_nick"]'
+
+    NAVIGATION_TAVERN = '//*[@id="ddcz_nav_tavern"]'
+    NAVIGATION_PHORUM = '//*[@id="ddcz_nav_phorum"]'
 
 
 class SeleniumTestCase(StaticLiveServerTestCase):
     # We are always connect to all interfaces
     # to simplify the local and CI setup
     host = "0.0.0.0"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.main_page_nav = MainPage
 
     @classproperty
     def live_server_url(cls):
@@ -38,3 +56,47 @@ class SeleniumTestCase(StaticLiveServerTestCase):
     def tearDownClass(cls):
         cls.selenium.quit()
         super().tearDownClass()
+
+    ###
+    # Helper methods for navigating the specific DDCZ webpage
+    ###
+
+    def navigate_as_authenticated_user(
+        self, user_profile, *, navigation_element, expected_title
+    ):
+        already_correct = False
+        if self.is_logged_in():
+            nick = self.el(MainPage.CONTROL_NICK).text
+            if nick == user_profile.nick:
+                already_correct = True
+            else:
+                self.el(MainPage.LOGOUT_SUBMIT).submit()
+
+        if not already_correct:
+            self.el(MainPage.LOGIN_USERNAME_INPUT).send_keys(user_profile.user.username)
+            self.el(MainPage.LOGIN_PASSWORD_INPUT).send_keys(user_profile.user.email)
+            self.el(MainPage.LOGIN_SUBMIT).submit()
+
+        self.assertEquals(
+            user_profile.nick,
+            self.el(MainPage.CONTROL_NICK).text,
+        )
+
+        self.el(navigation_element).click()
+        self.assertEquals(
+            expected_title,
+            self.el(MainPage.MAIN_TITLE).text,
+        )
+
+    ###
+    # Helper methods to retrieve information from the current page
+    ###
+
+    def el(self, enum):
+        return self.selenium.find_element_by_xpath(enum.value)
+
+    def els(self, enum):
+        return self.selenium.find_elements_by_xpath(enum.value)
+
+    def is_logged_in(self):
+        return self.el(MainPage.BODY).get_attribute("data-logged-in") == "1"
--- ddcz/tests/test_ui/test_tavern/test_listing.py
+++ ddcz/tests/test_ui/test_tavern/test_listing.py
@@ -3,7 +3,7 @@ from enum import Enum
 from django.db import transaction
 
 from ddcz.tavern import LIST_ALL
-from ..cases import SeleniumTestCase
+from ..cases import SeleniumTestCase, MainPage
 from ...model_generator import get_alphabetic_user_profiles, get_tavern_tables
 
 
@@ -11,18 +11,6 @@ class TavernTableListPage(Enum):
     URL = "/putyka/"
     TAVERN_TABLE_LIST_NAME = "//table[contains(@class, 'tavern-table-list')]//span[contains(@class, 'tavern-table-name')]"
     NAVIGATION_LIST_STYLE_TEMPLATE = "//a[@data-list-style='{slug}']"
-
-
-class MainPage(Enum):
-    BODY = "//body"
-    MAIN_TITLE = "//h1[contains(@class, 'page-heading')]"
-    LOGIN_USERNAME_INPUT = '//*[@id="id_nick"]'
-    LOGIN_PASSWORD_INPUT = '//*[@id="id_password"]'
-    LOGIN_SUBMIT = '//*[@id="login_submit"]'
-    LOGOUT_SUBMIT = '//*[@id="logout_submit"]'
-    CONTROL_NICK = '//*[@id="ddcz_nick"]'
-
-    NAVIGATION_TAVERN = '//*[@id="ddcz_nav_tavern"]'
 
 
 class TestTavernListing(SeleniumTestCase):
@@ -47,37 +35,11 @@ class TestTavernListing(SeleniumTestCase):
 
         self.selenium.get(self.live_server_url)
 
-    def el(self, enum):
-        return self.selenium.find_element_by_xpath(enum.value)
-
-    def els(self, enum):
-        return self.selenium.find_elements_by_xpath(enum.value)
-
-    def is_logged_in(self):
-        return self.el(MainPage.BODY).get_attribute("data-logged-in") == "1"
-
-    def navigate_as_user(self, user_profile):
-        already_correct = False
-        if self.is_logged_in():
-            nick = self.el(MainPage.CONTROL_NICK).text
-            if nick == user_profile.nick:
-                already_correct = True
-            else:
-                self.el(MainPage.LOGOUT_SUBMIT).submit()
-
-        if not already_correct:
-            self.el(MainPage.LOGIN_USERNAME_INPUT).send_keys(user_profile.user.username)
-            self.el(MainPage.LOGIN_PASSWORD_INPUT).send_keys(user_profile.user.email)
-            self.el(MainPage.LOGIN_SUBMIT).submit()
-
-        self.assertEquals(
-            user_profile.nick,
-            self.el(MainPage.CONTROL_NICK).text,
-        )
-        self.el(MainPage.NAVIGATION_TAVERN).click()
-        self.assertEquals(
-            "Putyka",
-            self.el(MainPage.MAIN_TITLE).text,
+    def navigate_to_tavern(self, user_profile):
+        return self.navigate_as_authenticated_user(
+            user_profile,
+            navigation_element=MainPage.NAVIGATION_TAVERN,
+            expected_title="Putyka",
         )
 
     def select_listing(self, listing):
@@ -96,7 +58,7 @@ class TestTavernListing(SeleniumTestCase):
             self.assertIn(table.name, rendered_table_names)
 
     def test_owner_sees_bookmarks(self):
-        self.navigate_as_user(self.owner)
+        self.navigate_to_tavern(self.owner)
 
         self.assertTablesInListing(
             [
@@ -106,13 +68,13 @@ class TestTavernListing(SeleniumTestCase):
         )
 
     def test_owner_sees_everything(self):
-        self.navigate_as_user(self.owner)
+        self.navigate_to_tavern(self.owner)
         self.select_listing(LIST_ALL)
 
         self.assertTablesInListing(self.tables.values())
 
     def test_allowed_user_sees_bookmarks(self):
-        self.navigate_as_user(self.allowed_user)
+        self.navigate_to_tavern(self.allowed_user)
 
         self.assertTablesInListing(
             [
@@ -122,7 +84,7 @@ class TestTavernListing(SeleniumTestCase):
         )
 
     def test_allowed_user_sees_everything(self):
-        self.navigate_as_user(self.allowed_user)
+        self.navigate_to_tavern(self.allowed_user)
         self.select_listing(LIST_ALL)
 
         self.assertTablesInListing(self.tables.values())
@@ -142,7 +104,7 @@ class TestTavernListing(SeleniumTestCase):
     #     )
 
     def test_visiting_user_sees_public_bookmarked(self):
-        self.navigate_as_user(self.visiting_user)
+        self.navigate_to_tavern(self.visiting_user)
         self.select_listing(LIST_ALL)
 
         self.assertTablesInListing(
