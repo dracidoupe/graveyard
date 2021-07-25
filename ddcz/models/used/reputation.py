f604a62de2e1f7c84f00cd839a13311bf2abe307
Almad <almad@dracidoupe.cz>
2021-07-25 13:23:08 +0200

    Fix reputation migrations (fixes #307, refs #126)

--- ddcz/models/used/reputation.py
+++ ddcz/models/used/reputation.py
@@ -1,15 +1,20 @@
 from django.db import models
 
-from ..magic import MisencodedCharField, MisencodedTextField
+from ..magic import MisencodedCharField
 
 
 class ReputationLog(models.Model):
     id = models.AutoField(primary_key=True, db_column="id_zaznamu")
-    donor = models.CharField(max_length=30, db_column="dal")
-    donee = models.CharField(max_length=30, db_column="prijal")
-    action = models.CharField(max_length=3, db_column="akce")
-    discussion = models.CharField(
-        max_length=1, blank=True, null=True, db_column="v_diskuzi"
+    donor = MisencodedCharField(max_length=30, db_column="dal")
+    donee = MisencodedCharField(max_length=30, db_column="prijal")
+    action = MisencodedCharField(max_length=3, db_column="akce")
+    # Note: this is a ChoiceField: enum('a','n','z','s','f')
+    # a = article discussion
+    # n = Tavern
+    # f = Phorum
+    # z and s needs research, candidates are s for special and z for Golden Dragon Award
+    discussion = MisencodedCharField(
+        max_length=1, blank=True, null=True, db_column="v_diskusi"
     )
     post = models.PositiveIntegerField(blank=True, null=True, db_column="id_prispevku")
     date = models.IntegerField()
@@ -19,8 +24,8 @@ class ReputationLog(models.Model):
 
 
 class ReputationAdditional(models.Model):
-    donee = models.CharField(max_length=25, db_column="prijal_nick")
-    reason = models.CharField(max_length=200, db_column="duvod_udeleni")
+    donee = MisencodedCharField(max_length=25, db_column="prijal_nick")
+    reason = MisencodedCharField(max_length=200, db_column="duvod_udeleni")
     amount = models.IntegerField(db_column="hodnota")
 
     class Meta:
