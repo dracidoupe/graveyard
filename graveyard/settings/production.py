7bc081ca462df6b5a7bcf317ffd27b5c73337129
Almad <almad@dracidoupe.cz>
2021-07-26 15:19:36 +0200

    Disable memcache on prod (refs #339)

--- graveyard/settings/production.py
+++ graveyard/settings/production.py
@@ -33,46 +33,46 @@ DATABASES = {
     }
 }
 
-if "MEMCACHIER_SERVERS" in os.environ:
-    logger.info("Using memcachier servers")
+# if "MEMCACHIER_SERVERS" in os.environ:
+#     logger.info("Using memcachier servers")
 
 KEY_PREFIX = "gy-"
 
 # See https://www.memcachier.com/documentation/django
-CACHES = {
-    "default": {
-        # Use pylibmc
-        "BACKEND": "django.core.cache.backends.memcached.PyLibMCCache",
-        # TIMEOUT is not the connection timeout! It's the default expiration
-        # timeout that should be applied to keys! Setting it to `None`
-        # disables expiration.
-        "TIMEOUT": None,
-        "LOCATION": os.environ["MEMCACHIER_SERVERS"],
-        "OPTIONS": {
-            # Use binary memcache protocol (needed for authentication)
-            "binary": True,
-            "username": os.environ["MEMCACHIER_USERNAME"],
-            "password": os.environ["MEMCACHIER_PASSWORD"],
-            "behaviors": {
-                # Enable faster IO
-                "no_block": True,
-                "tcp_nodelay": True,
-                # Keep connection alive
-                "tcp_keepalive": True,
-                # Timeout settings
-                "connect_timeout": 2000,  # ms
-                "send_timeout": 750 * 1000,  # us
-                "receive_timeout": 750 * 1000,  # us
-                "_poll_timeout": 2000,  # ms
-                # Better failover
-                "ketama": True,
-                "remove_failed": 1,
-                "retry_timeout": 2,
-                "dead_timeout": 30,
-            },
-        },
-    }
-}
+# CACHES = {
+#     "default": {
+#         # Use pylibmc
+#         "BACKEND": "django.core.cache.backends.memcached.PyLibMCCache",
+#         # TIMEOUT is not the connection timeout! It's the default expiration
+#         # timeout that should be applied to keys! Setting it to `None`
+#         # disables expiration.
+#         "TIMEOUT": None,
+#         "LOCATION": os.environ["MEMCACHIER_SERVERS"],
+#         "OPTIONS": {
+#             # Use binary memcache protocol (needed for authentication)
+#             "binary": True,
+#             "username": os.environ["MEMCACHIER_USERNAME"],
+#             "password": os.environ["MEMCACHIER_PASSWORD"],
+#             "behaviors": {
+#                 # Enable faster IO
+#                 "no_block": True,
+#                 "tcp_nodelay": True,
+#                 # Keep connection alive
+#                 "tcp_keepalive": True,
+#                 # Timeout settings
+#                 "connect_timeout": 2000,  # ms
+#                 "send_timeout": 750 * 1000,  # us
+#                 "receive_timeout": 750 * 1000,  # us
+#                 "_poll_timeout": 2000,  # ms
+#                 # Better failover
+#                 "ketama": True,
+#                 "remove_failed": 1,
+#                 "retry_timeout": 2,
+#                 "dead_timeout": 30,
+#             },
+#         },
+#     }
+# }
 
 
 WSGI_APPLICATION = "graveyard.wsgi.application"
