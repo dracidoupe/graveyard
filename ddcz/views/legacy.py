84fc37ffa7ae1c956c394b013b08a4497deae7d2
Jimmy Found <jimmeak.found@gmail.com>
2021-07-31 21:07:59 +0200

    New logging (#11)

--- ddcz/views/legacy.py
+++ ddcz/views/legacy.py
@@ -1,4 +1,3 @@
-import logging
 from django.apps import apps
 from ddcz.models.used.creations import Creation, CreativePage
 from django.http import HttpResponseRedirect
@@ -8,12 +7,14 @@ from django.urls.base import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.http import require_http_methods
 
+from ..legacy import log_wrong_legacy_route
+
 # In the constant below: It is possible to vary the redirections.
 # It is preferable to do so via the constants rather than changing
 # the code if there is no real need for it, so the legacy router
 # remains automatized.
 
-CREATION = ["prispevky", "prispevky_komp"]
+CREATION_LIST = ["prispevky", "prispevky_komp"]
 CREATION_DETAIL = "prispevky_precti"
 
 # Forbidden dictionary keys for PAGE_TO_VIEW_MAP:
@@ -61,61 +62,61 @@ ALLOWED_CREATION_PAGES = [
 ]
 
 
-@require_http_methods(["GET"])
+@require_http_methods(["HEAD", "GET"])
 def legacy_router(request):
 
-    section = request.GET.get("rub", False)
-    subsection = request.GET.get("co", False)
+    print("\n")
+    print(request.get_full_path())
+    print("\n")
+
+    page_category = request.GET.get("rub", False)
+    page_creation_type = request.GET.get("co", False)
     id = request.GET.get("id", False)
 
     # The LEGACY_PLAIN_ROUTER is redirecting basic pages.
     # Typically no creative pages are present here.
-    if section in PAGE_TO_VIEW_MAP.keys():
-        return HttpResponseRedirect(reverse(PAGE_TO_VIEW_MAP[section]))
+    if page_category in PAGE_TO_VIEW_MAP.keys():
+        return HttpResponseRedirect(reverse(PAGE_TO_VIEW_MAP[page_category]))
 
     # Some of the creation have legacy url
     # index.php?rub=prispevky(_komp)&co=page_slug
     # Here are lists of such creative pages.
-    if section in CREATION and subsection in COMMON_ARTICLES_NAME_MAP.keys():
+    if (
+        page_category in CREATION_LIST
+        and page_creation_type in COMMON_ARTICLES_NAME_MAP.keys()
+    ):
         return HttpResponseRedirect(
             reverse(
                 "ddcz:creation-list",
-                kwargs={"creative_page_slug": COMMON_ARTICLES_NAME_MAP[subsection]},
+                kwargs={
+                    "creative_page_slug": COMMON_ARTICLES_NAME_MAP[page_creation_type]
+                },
             )
         )
 
     # For index.php?rub=prispevky_jeden&subsection=page_slug&id=article_id
     # we can find the article detail by Id.
-    if section == CREATION_DETAIL and id is not False:
-        page = get_object_or_404(CreativePage, slug=subsection)
+    if page_category == CREATION_DETAIL and id is not False:
+        page = get_object_or_404(CreativePage, slug=page_creation_type)
         return get_creation_detail_redirect(page, id)
 
     # There are some special creative pages that are not stored
     # as the others, those have their own tables in the database.
     # For those we have lists and details in this for loop.
     for name in ALLOWED_CREATION_PAGES:
-        if section in [name, name + "_komp"]:
+        if page_category in [name, name + "_komp"]:
             return HttpResponseRedirect(
                 reverse(
                     "ddcz:creation-list",
                     kwargs={"creative_page_slug": name},
                 )
             )
-        if section in [name + "_jeden"]:
+        if page_category in [name + "_jeden"]:
             page = get_object_or_404(CreativePage, slug=name)
             return get_creation_detail_redirect(page, id)
 
-    ###  Finally if no route is found, redirect to news
-    logger = logging.getLogger(__name__)
-    logger.info(
-        "There has been submitted URL address from the old website: index.php | rub: "
-        + section
-        + ", co: "
-        + subsection
-        + ", id: "
-        + id
-        + "."
-    )
+    ###  Finally if no route is found, redirect to news and log
+    log_wrong_legacy_route(request.get_full_path())
     return HttpResponseRedirect(reverse("ddcz:news"))
 
 
