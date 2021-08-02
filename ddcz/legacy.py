556afb34dd29918259e8e147ef1c1be4ae8f87a5
Jimmy Found <jimmeak.found@gmail.com>
2021-08-02 21:27:29 +0200

    Info changed to warning

--- ddcz/legacy.py
+++ ddcz/legacy.py
@@ -5,6 +5,6 @@ logger = logging.getLogger(__name__)
 
 
 def log_wrong_legacy_route(url):
-    logger.info(
+    logger.warning(
         f"There has been submitted URL address from the old website: index.php >> No redirect could be found for a legacy URL {url}"
     )
