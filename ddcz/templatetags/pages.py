6215c4c7dce8af8e593fc6de9bc9df9c79881345
Jimmy Found <jimmeak.found@gmail.com>
2021-08-14 23:48:44 +0200

    Postal Service | Pagination

--- ddcz/templatetags/pages.py
+++ ddcz/templatetags/pages.py
@@ -1,10 +1,9 @@
 from django import template
+from django.http import request
 
 register = template.Library()
 
 
 @register.inclusion_tag("creative-pages/pagination-links.html")
-def pagination(articles):
-    return {
-        "articles": articles,
-    }
+def pagination(articles, per_page=False):
+    return {"articles": articles, "per_page": per_page}
