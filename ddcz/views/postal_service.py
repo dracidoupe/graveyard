74e23c6c4da1644182208028683a3816358b7b39
Jimmy Found <jimmeak.found@gmail.com>
2021-08-14 23:48:44 +0200

    Postal Service | Pagination

--- ddcz/views/postal_service.py
+++ ddcz/views/postal_service.py
@@ -20,6 +20,9 @@ FORM_DELETE = 1
 FORM_SEND = 2
 FORM_REPLY = 4
 
+DEFAULT_LIMIT = 10
+DEFAULT_PAGE = 1
+
 logger = logging.getLogger(__name__)
 
 
@@ -27,53 +30,21 @@ logger = logging.getLogger(__name__)
 @require_http_methods(["HEAD", "GET", "POST"])
 def postal_service(request):
     if request.method == "POST":
-
-        fid = int(request.POST.get("fid"))
-        if fid == FORM_SEND:
-
-            try:
-                Letters.objects.create(
-                    receiver=UserProfile.objects.get(id=request.POST.get("whom")).nick,
-                    sender=request.user.userprofile.nick,
-                    text=request.POST.get("text"),
-                    date=datetime.now(),
-                    visibility=1,
-                )
-                return HttpResponseRedirect(reverse("ddcz:postal-service"))
-
-            except UserProfile.DoesNotExist:
-                id = request.POST.get("whom")
-                logger.error(
-                    f"A message between user has been submitted but the receiver of userprofile can not be found in our database. ID: {id}"
-                )
-
-        elif fid == FORM_DELETE:
-
-            try:
-                letter = Letters.objects.filter(pk=request.POST.get("id", 0)).update(
-                    visibility=0
-                )
-                return HttpResponseRedirect(reverse("ddcz:postal-service"))
-
-            except Letters.DoesNotExist:
-                id = request.POST.get("id")
-                user = request.user.userprofile.nick
-                logger.error(
-                    f"There has been an attempt to delete a message with non existing id: {id}, User: {user}"
-                )
-                return HttpResponseRedirect(reverse("ddcz:postal-service"))
+        return handle_postal_service_post_request(request)
 
     nick = request.user.userprofile.nick
-    limit = int(request.GET.get("l", 50))
-    page = int(request.GET.get("p", 1))
+    per_page = int(request.GET.get("l", DEFAULT_LIMIT))
+    page = int(request.GET.get("z_s", DEFAULT_PAGE))
+
     letters = Letters.objects.filter(
         (Q(receiver=nick) | Q(sender=nick)) & Q(visibility=1)
     ).order_by("-date")
+
     box_occupancy = Letters.objects.filter(
         (Q(receiver=nick) | Q(sender=nick)) & Q(visibility=1)
     ).count()
 
-    paginator = Paginator(letters, limit)
+    paginator = Paginator(letters, per_page)
     letters = paginator.get_page(page)
 
     return render(
@@ -83,8 +54,44 @@ def postal_service(request):
             "reply_id": FORM_REPLY,
             "send_id": FORM_SEND,
             "delete_id": FORM_DELETE,
-            "users": UserProfile.objects.all(),
+            "users": UserProfile.objects.all(),  # this may be too much for select tag.
             "letters": letters,
+            "per_page": per_page if per_page != DEFAULT_LIMIT else False,
             "box_occupancy": box_occupancy,
         },
     )
+
+
+def handle_postal_service_post_request(request):
+    fid = int(request.POST.get("fid"))
+    if fid == FORM_SEND:
+        try:
+            Letters.objects.create(
+                receiver=UserProfile.objects.get(id=request.POST.get("whom")).nick,
+                sender=request.user.userprofile.nick,
+                text=request.POST.get("text"),
+                date=datetime.now(),
+                visibility=1,
+            )
+            return HttpResponseRedirect(reverse("ddcz:postal-service"))
+        except UserProfile.DoesNotExist:
+            id = request.POST.get("whom")
+            logger.error(
+                f"A message between user has been submitted but the receiver of userprofile can not be found in our database. ID: {id}"
+            )
+            return HttpResponseRedirect(reverse("ddcz:postal-service"))
+
+    elif fid == FORM_DELETE:
+
+        try:
+            letter = Letters.objects.filter(pk=request.POST.get("id", 0)).update(
+                visibility=0
+            )
+            return HttpResponseRedirect(reverse("ddcz:postal-service"))
+        except Letters.DoesNotExist:
+            id = request.POST.get("id")
+            user = request.user.userprofile.nick
+            logger.error(
+                f"There has been an attempt to delete a message with non existing id: {id}, User: {user}"
+            )
+            return HttpResponseRedirect(reverse("ddcz:postal-service"))
