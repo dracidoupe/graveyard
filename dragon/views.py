e87392e98f59e9321a39f638bdc78420c4af0ad2
Almad <almad@dracidoupe.cz>
2021-08-19 15:45:09 +0200

    Add page for testing email delivery (refs #90)

--- dragon/views.py
+++ dragon/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.conf import settings
 from django.contrib.admin.views import decorators
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render
 
-from ddcz.models import LevelSystemParams
+from ddcz.models import LevelSystemParams, UserProfile
 
 
 @decorators.staff_member_required()
@@ -16,3 +19,20 @@ def levelsystem(request):
     params = LevelSystemParams.objects.all()
 
     return render(request, "levelsystem/view.html", {"level_params": params})
+
+
+@decorators.staff_member_required()
+def emailtest(request):
+    if request.method == "POST":
+        profile = UserProfile.objects.get(user_id=request.user.id)
+        send_mail(
+            "Testovací e-mail redakčního systému DraciDoupe.cz",
+            "Vše funguje, jak má!",
+            settings.DDCZ_TRANSACTION_EMAIL_FROM,
+            [profile.email],
+        )
+        messages.add_message(
+            request, messages.SUCCESS, "Testovací e-mail byl v pořádku odeslán."
+        )
+
+    return render(request, "emailtest.html")
