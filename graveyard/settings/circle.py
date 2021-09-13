a050bf20b356f8926b43f6cf095db62c7eae7c1f
Almad <almad@dracidoupe.cz>
2021-09-13 14:58:20 +0200

    Revert "chore: Remove CircleCI config"
    This reverts commit ce085dad362248d63ebe890008c87d1014324dc8.

---
+++ graveyard/settings/circle.py
@@ -0,0 +1,30 @@
+import socket
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "circle_test",
+        "USER": "root",
+        "HOST": "db",
+        "PASSWORD": "docker",
+        "OPTIONS": {"charset": "latin2"},
+        "TEST": {
+            "NAME": "circle_test",
+            "CHARSET": "latin2",
+        },
+    }
+}
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+SELENIUM_HUB_HOST = "selenium-hub"
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    socket.gethostname(),
+]
