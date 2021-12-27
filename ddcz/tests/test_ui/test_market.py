359b2d6e4e2b9c8c99640e0b6f315ec11a5d3dd8
Almad <almad@dracidoupe.cz>
2021-12-27 17:38:26 +0100

    Use Python 3.7.12 everywhere for consistency

---
+++ ddcz/tests/test_ui/test_market.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+from .cases import SeleniumTestCase, MainPage
+from ..attack_strings import SCRIPT_ALERT_INPUT
+from ..model_generator import create_market_entries
+
+
+class MarketPage(Enum):
+    FIRST_SECTION_LINK = '//div[@id=page-market"]//article[@class="market]//a'
+    # POST_TEXTAREA = '//textarea[@id="id_text"]'
+    # POST_SUBMIT = '//*[@value="Přidej"]'
+
+
+class TestMarket(SeleniumTestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.entries = create_market_entries()
+        self.first_entry = self.entries[0]
+
+        self.selenium.get("%s%s" % (self.live_server_url, "/"))
+        self.navigate_to_market()
+
+    def navigate_to_market(self):
+        self.el(MainPage.NAVIGATION_MARKET).click()
+        self.assertEquals(
+            "Inzerce",
+            self.el(MainPage.MAIN_TITLE).text,
+        )
+
+    def test_page_heading_present(self):
+        text = self.selenium.find_element_by_xpath('//h1[@class="page-heading"]').text
+        self.assertEquals("Market", text)
+
+    def test_entry_text_rendered(self):
+        text = self.selenium.find_element_by_xpath(
+            '//div[@id="page-market"]//p[@class="comment_text"]'
+        ).text
+        self.assertEquals(SCRIPT_ALERT_INPUT, text)
