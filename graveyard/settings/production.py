cd61169e53b36e466a2d2a1cbe85089bfec4413f
Almad <almad@dracidoupe.cz>
2024-03-20 19:43:47 +0100

    Roll back CF link due to some ico/svg not working (refs #463)

--- graveyard/settings/production.py
+++ graveyard/settings/production.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = "graveyard.wsgi.application"
 ALLOWED_HOSTS = ["www.dracidoupe.cz", "nove.dracidoupe.cz"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "https://static.dracidoupe.cz/"
+STATIC_URL = "https://www.dracidoupe.cz/staticfiles/"
 
 # On Django 4.2+:
 # STORAGES = {
