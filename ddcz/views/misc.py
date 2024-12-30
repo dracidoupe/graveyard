424b6b52443f7b658c6e44242c1e292934f7ddf4
Almad <almad@dracidoupe.cz>
2024-12-30 01:16:49 +0100

    Fix imports

--- ddcz/views/misc.py
+++ ddcz/views/misc.py
@@ -4,10 +4,16 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
 from django.views.decorators.http import require_http_methods
 
-from ..models import Market
+from ..models import (
+    Market,
+    UserProfile,
+    Dating,
+    Link,
+    EditorArticle,
+    MARKET_SECTION_CHOICES,
+)
 from ..forms.market import MarketForm
 
 DEFAULT_LIST_SIZE = 10
