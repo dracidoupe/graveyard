59d916536743cd54c3a0b56ed30eb95c073c760d
Almad <almad@dracidoupe.cz>
2025-01-20 00:25:27 +0100

    Ruff it

--- ddcz/migrations/0011_dating_autopublish_20250116_2222.py
+++ ddcz/migrations/0011_dating_autopublish_20250116_2222.py
@@ -4,15 +4,16 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('ddcz', '0010_dating_20241230_1327'),
+        ("ddcz", "0010_dating_20241230_1327"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='dating',
-            name='published',
-            field=models.DateTimeField(auto_now_add=True, db_column='datum', null=True, verbose_name='Datum'),
+            model_name="dating",
+            name="published",
+            field=models.DateTimeField(
+                auto_now_add=True, db_column="datum", null=True, verbose_name="Datum"
+            ),
         ),
     ]
