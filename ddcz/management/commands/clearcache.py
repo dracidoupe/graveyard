dc3563dc8a1e9a43d164b3c7b67dc5ecab463eb3
Almad <almad@dracidoupe.cz>
2026-06-27 19:25:36 -0700

    add clearcache

---
+++ ddcz/management/commands/clearcache.py
@@ -0,0 +1,10 @@
+from django.core.cache import cache
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Clear the default cache"
+
+    def handle(self, *args, **options):
+        cache.clear()
+        self.stdout.write("Cache cleared.")
